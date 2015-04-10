958551fd3f631ac210e6a20a03f2130d82804be1
hsavit1 <henrysavit@gmail.com>
2015-04-10 13:31:42 -0400

    Initial Commit

---
+++ AbstractFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AbstractFactory
+//
+//  Created by Henry Savit on 4/10/15.
+//  Copyright (c) 2015 Henry Savit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AbstractFactoryTests/AbstractFactoryTests.m
@@ -0,0 +1,40 @@
+//
+//  AbstractFactoryTests.m
+//  AbstractFactoryTests
+//
+//  Created by Henry Savit on 4/10/15.
+//  Copyright (c) 2015 Henry Savit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AbstractFactoryTests : XCTestCase
+
+@end
+
+@implementation AbstractFactoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
